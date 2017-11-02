28cca159e5b496358bf35f5ed142d8e25bf6c538
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-11-02 10:50:47 +0000

    Comment ISRs

--- arch/isr.asm
+++ arch/isr.asm
@@ -17,38 +17,38 @@ isr%1:
     jmp isr_common_stub
 %endmacro
 
-ISR_NOERRCODE 0
-ISR_NOERRCODE 1
-ISR_NOERRCODE 2
-ISR_NOERRCODE 3
-ISR_NOERRCODE 4
-ISR_NOERRCODE 5
-ISR_NOERRCODE 6
-ISR_NOERRCODE 7
-ISR_ERRCODE   8
-ISR_NOERRCODE 9
-ISR_ERRCODE   10
-ISR_ERRCODE   11
-ISR_ERRCODE   12
-ISR_ERRCODE   13
-ISR_ERRCODE   14
-ISR_NOERRCODE 15
-ISR_NOERRCODE 16
-ISR_ERRCODE   17
-ISR_NOERRCODE 18
-ISR_NOERRCODE 19
-ISR_NOERRCODE 20
-ISR_NOERRCODE 21
-ISR_NOERRCODE 22
-ISR_NOERRCODE 23
-ISR_NOERRCODE 24
-ISR_NOERRCODE 25
-ISR_NOERRCODE 26
-ISR_NOERRCODE 27
-ISR_NOERRCODE 28
-ISR_NOERRCODE 29
-ISR_NOERRCODE 30
-ISR_NOERRCODE 31
+ISR_NOERRCODE 0  ; #DE - Divide Error Exception
+ISR_NOERRCODE 1  ; #DB - Debug Exception
+ISR_NOERRCODE 2  ; NMI - Non-maskable interrupt
+ISR_NOERRCODE 3  ; #BP - Breakpoint Exception
+ISR_NOERRCODE 4  ; #OF - Overflow Exception
+ISR_NOERRCODE 5  ; #BR - BOUND Range Exceeded Exception
+ISR_NOERRCODE 6  ; #UD - Invalid Opcode Exception
+ISR_NOERRCODE 7  ; #NM - Device Not Available Exception
+ISR_ERRCODE   8  ; #DF - Double Fault Exception
+ISR_NOERRCODE 9  ; Reserved - Coprocessor Segment Overrun
+ISR_ERRCODE   10 ; #TS - Invalid TSS Exception
+ISR_ERRCODE   11 ; #NP - Segment Not Present
+ISR_ERRCODE   12 ; #SS - Stack Fault Exception
+ISR_ERRCODE   13 ; #GP - General Protection Exception
+ISR_ERRCODE   14 ; #PF - Page-Fault Exception
+ISR_NOERRCODE 15 ; Reserved
+ISR_NOERRCODE 16 ; #MF - x87 FPU Floating-Point Error
+ISR_ERRCODE   17 ; #AC - Alignment Check Exception
+ISR_NOERRCODE 18 ; #MC - Machine-Check Exception
+ISR_NOERRCODE 19 ; #XF - SIMD Floating-Point Exception
+ISR_NOERRCODE 20 ; Reserved
+ISR_NOERRCODE 21 ; Reserved
+ISR_NOERRCODE 22 ; Reserved
+ISR_NOERRCODE 23 ; Reserved
+ISR_NOERRCODE 24 ; Reserved
+ISR_NOERRCODE 25 ; Reserved
+ISR_NOERRCODE 26 ; Reserved
+ISR_NOERRCODE 27 ; Reserved
+ISR_NOERRCODE 28 ; Reserved
+ISR_NOERRCODE 29 ; Reserved
+ISR_NOERRCODE 30 ; Reserved
+ISR_NOERRCODE 31 ; Reserved
 
 isr_common_stub:
    pusha ; Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
