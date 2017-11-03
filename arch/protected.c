7ffcc99755dc537ae85d0a29669369281d612fda
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-11-03 02:20:42 +0000

    Add file "arch/exception.h"

--- arch/protected.c
+++ arch/protected.c
@@ -2,6 +2,7 @@
 
 #include "config.h"
 #include "logger.h"
+#include "exception.h"
 
 struct GdtPointer {
     unsigned short limit;
@@ -46,39 +47,6 @@ static void idt_set_gate(unsigned char num, unsigned int base, unsigned short se
 
 void gdt_flush(const struct GdtPointer *pointer);
 void idt_flush(const struct IdtPointer *pointer);
-
-void isr0();
-void isr1();
-void isr2();
-void isr3();
-void isr4();
-void isr5();
-void isr6();
-void isr7();
-void isr8();
-void isr9();
-void isr10();
-void isr11();
-void isr12();
-void isr13();
-void isr14();
-void isr15();
-void isr16();
-void isr17();
-void isr18();
-void isr19();
-void isr20();
-void isr21();
-void isr22();
-void isr23();
-void isr24();
-void isr25();
-void isr26();
-void isr27();
-void isr28();
-void isr29();
-void isr30();
-void isr31();
 
 void protected_initialize()
 {
