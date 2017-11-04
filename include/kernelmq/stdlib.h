48ff83b5a3a7f5b39adb7df20ce4be3da59f283f
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-11-04 09:10:12 +0000

    Add guard for C++

--- include/kernelmq/stdlib.h
+++ include/kernelmq/stdlib.h
@@ -1,8 +1,16 @@
 #ifndef KERNELMQ_INCLUDED_STDLIB
 #define KERNELMQ_INCLUDED_STDLIB 1
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void kmemset(void *buffer, unsigned char value, unsigned int size);
 unsigned int kstrlen(const char *s);
 void kitoa(char *buf, int base, int d);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
