339992a503a12229e36d0127e27998cb2ecb9b32
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-11-04 03:35:54 +0000

    Display hex in uppercase

--- arch/util.h
+++ arch/util.h
@@ -35,7 +35,7 @@ void itoa(char *buf, int base, int d)
     // Divide UD by DIVISOR until UD == 0.
     do {
         int remainder = ud % divisor;
-        *p++ = (remainder < 10) ? remainder + '0' : remainder + 'a' - 10;
+        *p++ = (remainder < 10) ? remainder + '0' : remainder + 'A' - 10;
     }
     while (ud /= divisor);
 
