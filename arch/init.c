70bcb07df3995f70954829da59c2d9f0c1f09dfc
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-11-04 10:19:53 +0000

    Remove memory module

--- arch/init.c
+++ arch/init.c
@@ -1,14 +1,27 @@
 #include "console.h"
 #include "logger.h"
+#include "kprintf.h"
 #include "multiboot.h"
-#include "memory.h"
 
 #include <kernelmq/info.h>
+#include <kernelmq/stdlib.h>
+
+// Defined in linker script
+extern char _kernel_offset;
+extern char _kernel_phys_base;
+extern char _kernel_virt_base;
 
 static struct KernelMQ_Info kinfo;
 
 void init(struct KernelMQ_Multiboot_Info multiboot_info)
 {
+    kmemset(&kinfo, 0, sizeof(struct KernelMQ_Info));
+
+    kinfo.kernel_offset = (unsigned long)&_kernel_offset;
+
+    kinfo.kernel_phys_base = (unsigned long)&_kernel_phys_base;
+    kinfo.kernel_virt_base = (unsigned long)&_kernel_virt_base;
+
     console_initialize();
 
     logger_info("Multiboot info:");
@@ -17,5 +30,10 @@ void init(struct KernelMQ_Multiboot_Info multiboot_info)
 
     logger_info("Virtual memory info:");
 
-    print_memory_info();
+    kprintf(
+        "0x%x (phys base) + 0x%x (offset) = 0x%x (virt base)\n",
+        kinfo.kernel_phys_base,
+        kinfo.kernel_offset,
+        kinfo.kernel_virt_base
+    );
 }
