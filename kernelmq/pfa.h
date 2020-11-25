b3f37ba9312b6675b60541d301e7103e831e96a6
Alex Kotov <kotovalexarian@gmail.com>
2020-11-26 00:07:17 +0500

    Add extern "C"

--- kernelmq/pfa.h
+++ kernelmq/pfa.h
@@ -3,6 +3,10 @@
 
 #include "info.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void pfa_initialize(const struct KernelMQ_Info *kinfo);
 
 unsigned long pfa_alloc_page();
@@ -10,5 +14,9 @@ unsigned long pfa_alloc_big_page();
 
 void pfa_free_page(unsigned long base);
 void pfa_free_big_page(unsigned long base);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
