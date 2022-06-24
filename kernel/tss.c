aff583980befe2f6a7fb03d1ff6063e2b42188ad
Alex Kotov <kotovalexarian@gmail.com>
2022-06-24 06:04:51 +0300

    Use DTE and TSS from libkernaux

--- kernel/tss.c
+++ kernel/tss.c
@@ -5,41 +5,11 @@
 #include <kernaux/arch/i386.h>
 #include <kernaux/libc.h>
 
-struct tss_entry {
-    unsigned int prev_tss;
-    unsigned int esp0;
-    unsigned int ss0;
-    unsigned int esp1;
-    unsigned int ss1;
-    unsigned int esp2;
-    unsigned int ss2;
-    unsigned int cr3;
-    unsigned int eip;
-    unsigned int eflags;
-    unsigned int eax;
-    unsigned int ecx;
-    unsigned int edx;
-    unsigned int ebx;
-    unsigned int esp;
-    unsigned int ebp;
-    unsigned int esi;
-    unsigned int edi;
-    unsigned int es;
-    unsigned int cs;
-    unsigned int ss;
-    unsigned int ds;
-    unsigned int fs;
-    unsigned int gs;
-    unsigned int ldt;
-    unsigned short trap;
-    unsigned short iomap_base;
-};
-
-static struct tss_entry tss;
+static struct KernAux_Arch_I386_TSS tss;
 
 void tss_write_to_gdt(const struct Kernel_Info *const kinfo, void *gdt_entry_ptr)
 {
-    struct KernAux_Arch_I386_GDTE *const g = gdt_entry_ptr;
+    struct KernAux_Arch_I386_DTE *const g = gdt_entry_ptr;
 
     unsigned long base = (unsigned long)&tss;
     unsigned long limit = sizeof(tss);
