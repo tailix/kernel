7e0c990e2d6bcab0592c434a6dfd2aabfc542e1e
Alex Kotov <kotovalexarian@gmail.com>
2022-06-24 07:04:53 +0300

    Improve code

--- kernel/protected.c
+++ kernel/protected.c
@@ -36,6 +36,7 @@ static struct KernAux_Arch_I386_DTE gdt_entries[GDT_SIZE];
 static struct IdtEntry idt_entries[IDT_SIZE];
 
 static void gdt_set_gates();
+static void idt_set_gates();
 
 static void idt_set_gate(unsigned char num, unsigned int base, unsigned short sel, unsigned char flags);
 
@@ -53,63 +54,7 @@ void protected_initialize(const struct Kernel_Info *const kinfo)
     tss_write_to_gdt(kinfo, &gdt_entries[GDT_TSS_INDEX]);
 
     kernaux_drivers_console_print("[INFO] protected: Setup IDT.\n");
-
-    memset(idt_entries, 0, sizeof(idt_entries));
-
-    idt_set_gate(0,  (unsigned int)interrupt_0,  0x08, 0x8E);
-    idt_set_gate(1,  (unsigned int)interrupt_1,  0x08, 0x8E);
-    idt_set_gate(2,  (unsigned int)interrupt_2,  0x08, 0x8E);
-    idt_set_gate(3,  (unsigned int)interrupt_3,  0x08, 0x8E);
-    idt_set_gate(4,  (unsigned int)interrupt_4,  0x08, 0x8E);
-    idt_set_gate(5,  (unsigned int)interrupt_5,  0x08, 0x8E);
-    idt_set_gate(6,  (unsigned int)interrupt_6,  0x08, 0x8E);
-    idt_set_gate(7,  (unsigned int)interrupt_7,  0x08, 0x8E);
-    idt_set_gate(8,  (unsigned int)interrupt_8,  0x08, 0x8E);
-    idt_set_gate(9,  (unsigned int)interrupt_9,  0x08, 0x8E);
-    idt_set_gate(10, (unsigned int)interrupt_10, 0x08, 0x8E);
-    idt_set_gate(11, (unsigned int)interrupt_11, 0x08, 0x8E);
-    idt_set_gate(12, (unsigned int)interrupt_12, 0x08, 0x8E);
-    idt_set_gate(13, (unsigned int)interrupt_13, 0x08, 0x8E);
-    idt_set_gate(14, (unsigned int)interrupt_14, 0x08, 0x8E);
-    idt_set_gate(15, (unsigned int)interrupt_15, 0x08, 0x8E);
-    idt_set_gate(16, (unsigned int)interrupt_16, 0x08, 0x8E);
-    idt_set_gate(17, (unsigned int)interrupt_17, 0x08, 0x8E);
-    idt_set_gate(18, (unsigned int)interrupt_18, 0x08, 0x8E);
-    idt_set_gate(19, (unsigned int)interrupt_19, 0x08, 0x8E);
-    idt_set_gate(20, (unsigned int)interrupt_20, 0x08, 0x8E);
-    idt_set_gate(21, (unsigned int)interrupt_21, 0x08, 0x8E);
-    idt_set_gate(22, (unsigned int)interrupt_22, 0x08, 0x8E);
-    idt_set_gate(23, (unsigned int)interrupt_23, 0x08, 0x8E);
-    idt_set_gate(24, (unsigned int)interrupt_24, 0x08, 0x8E);
-    idt_set_gate(25, (unsigned int)interrupt_25, 0x08, 0x8E);
-    idt_set_gate(26, (unsigned int)interrupt_26, 0x08, 0x8E);
-    idt_set_gate(27, (unsigned int)interrupt_27, 0x08, 0x8E);
-    idt_set_gate(28, (unsigned int)interrupt_28, 0x08, 0x8E);
-    idt_set_gate(29, (unsigned int)interrupt_29, 0x08, 0x8E);
-    idt_set_gate(30, (unsigned int)interrupt_30, 0x08, 0x8E);
-    idt_set_gate(31, (unsigned int)interrupt_31, 0x08, 0x8E);
-
-    // Master PIC
-    idt_set_gate(32, (unsigned int)interrupt_32, 0x08, 0x8E);
-    idt_set_gate(33, (unsigned int)interrupt_33, 0x08, 0x8E);
-    idt_set_gate(34, (unsigned int)interrupt_34, 0x08, 0x8E);
-    idt_set_gate(35, (unsigned int)interrupt_35, 0x08, 0x8E);
-    idt_set_gate(36, (unsigned int)interrupt_36, 0x08, 0x8E);
-    idt_set_gate(37, (unsigned int)interrupt_37, 0x08, 0x8E);
-    idt_set_gate(38, (unsigned int)interrupt_38, 0x08, 0x8E);
-    idt_set_gate(39, (unsigned int)interrupt_39, 0x08, 0x8E);
-
-    // Slave PIC
-    idt_set_gate(40, (unsigned int)interrupt_40, 0x08, 0x8E);
-    idt_set_gate(41, (unsigned int)interrupt_41, 0x08, 0x8E);
-    idt_set_gate(42, (unsigned int)interrupt_42, 0x08, 0x8E);
-    idt_set_gate(43, (unsigned int)interrupt_43, 0x08, 0x8E);
-    idt_set_gate(44, (unsigned int)interrupt_44, 0x08, 0x8E);
-    idt_set_gate(45, (unsigned int)interrupt_45, 0x08, 0x8E);
-    idt_set_gate(46, (unsigned int)interrupt_46, 0x08, 0x8E);
-    idt_set_gate(47, (unsigned int)interrupt_47, 0x08, 0x8E);
-
-    idt_set_gate(INT_SYSCALL, (unsigned int)interrupt_0x80, 0x08, 0x8E | 0x60);
+    idt_set_gates();
 
     kernaux_drivers_console_print("[INFO] protected: Load GDT.\n");
 
@@ -211,6 +156,68 @@ void gdt_set_gates()
         .DPL                    = 3,
         .present                = 1,
     };
+}
+
+void idt_set_gates()
+{
+    memset(idt_entries, 0, sizeof(idt_entries));
+
+    // exception
+    idt_set_gate(0,  (unsigned int)interrupt_0,  0x08, 0x8E);
+    idt_set_gate(1,  (unsigned int)interrupt_1,  0x08, 0x8E);
+    idt_set_gate(2,  (unsigned int)interrupt_2,  0x08, 0x8E);
+    idt_set_gate(3,  (unsigned int)interrupt_3,  0x08, 0x8E);
+    idt_set_gate(4,  (unsigned int)interrupt_4,  0x08, 0x8E);
+    idt_set_gate(5,  (unsigned int)interrupt_5,  0x08, 0x8E);
+    idt_set_gate(6,  (unsigned int)interrupt_6,  0x08, 0x8E);
+    idt_set_gate(7,  (unsigned int)interrupt_7,  0x08, 0x8E);
+    idt_set_gate(8,  (unsigned int)interrupt_8,  0x08, 0x8E);
+    idt_set_gate(9,  (unsigned int)interrupt_9,  0x08, 0x8E);
+    idt_set_gate(10, (unsigned int)interrupt_10, 0x08, 0x8E);
+    idt_set_gate(11, (unsigned int)interrupt_11, 0x08, 0x8E);
+    idt_set_gate(12, (unsigned int)interrupt_12, 0x08, 0x8E);
+    idt_set_gate(13, (unsigned int)interrupt_13, 0x08, 0x8E);
+    idt_set_gate(14, (unsigned int)interrupt_14, 0x08, 0x8E);
+    idt_set_gate(15, (unsigned int)interrupt_15, 0x08, 0x8E);
+    idt_set_gate(16, (unsigned int)interrupt_16, 0x08, 0x8E);
+    idt_set_gate(17, (unsigned int)interrupt_17, 0x08, 0x8E);
+    idt_set_gate(18, (unsigned int)interrupt_18, 0x08, 0x8E);
+    idt_set_gate(19, (unsigned int)interrupt_19, 0x08, 0x8E);
+    idt_set_gate(20, (unsigned int)interrupt_20, 0x08, 0x8E);
+    idt_set_gate(21, (unsigned int)interrupt_21, 0x08, 0x8E);
+    idt_set_gate(22, (unsigned int)interrupt_22, 0x08, 0x8E);
+    idt_set_gate(23, (unsigned int)interrupt_23, 0x08, 0x8E);
+    idt_set_gate(24, (unsigned int)interrupt_24, 0x08, 0x8E);
+    idt_set_gate(25, (unsigned int)interrupt_25, 0x08, 0x8E);
+    idt_set_gate(26, (unsigned int)interrupt_26, 0x08, 0x8E);
+    idt_set_gate(27, (unsigned int)interrupt_27, 0x08, 0x8E);
+    idt_set_gate(28, (unsigned int)interrupt_28, 0x08, 0x8E);
+    idt_set_gate(29, (unsigned int)interrupt_29, 0x08, 0x8E);
+    idt_set_gate(30, (unsigned int)interrupt_30, 0x08, 0x8E);
+    idt_set_gate(31, (unsigned int)interrupt_31, 0x08, 0x8E);
+
+    // hwint: master PIC
+    idt_set_gate(32, (unsigned int)interrupt_32, 0x08, 0x8E);
+    idt_set_gate(33, (unsigned int)interrupt_33, 0x08, 0x8E);
+    idt_set_gate(34, (unsigned int)interrupt_34, 0x08, 0x8E);
+    idt_set_gate(35, (unsigned int)interrupt_35, 0x08, 0x8E);
+    idt_set_gate(36, (unsigned int)interrupt_36, 0x08, 0x8E);
+    idt_set_gate(37, (unsigned int)interrupt_37, 0x08, 0x8E);
+    idt_set_gate(38, (unsigned int)interrupt_38, 0x08, 0x8E);
+    idt_set_gate(39, (unsigned int)interrupt_39, 0x08, 0x8E);
+
+    // hwint: slave PIC
+    idt_set_gate(40, (unsigned int)interrupt_40, 0x08, 0x8E);
+    idt_set_gate(41, (unsigned int)interrupt_41, 0x08, 0x8E);
+    idt_set_gate(42, (unsigned int)interrupt_42, 0x08, 0x8E);
+    idt_set_gate(43, (unsigned int)interrupt_43, 0x08, 0x8E);
+    idt_set_gate(44, (unsigned int)interrupt_44, 0x08, 0x8E);
+    idt_set_gate(45, (unsigned int)interrupt_45, 0x08, 0x8E);
+    idt_set_gate(46, (unsigned int)interrupt_46, 0x08, 0x8E);
+    idt_set_gate(47, (unsigned int)interrupt_47, 0x08, 0x8E);
+
+    // syscall
+    idt_set_gate(INT_SYSCALL, (unsigned int)interrupt_0x80, 0x08, 0x8E | 0x60);
 }
 
 void idt_set_gate(unsigned char num, unsigned int base, unsigned short sel, unsigned char flags)
